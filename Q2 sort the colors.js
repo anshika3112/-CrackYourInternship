b82ca66e8a45476ca94369de1b95b874c0dea5c5
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-14 22:39:57 +0530

    Create Q2 sort the colors.js

---
+++ Q2 sort the colors.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    var n = nums.length;
+    var index = 0;
+    var left = 0;
+    var right = n - 1; // Corrected
+
+    while (index <= right) {
+        if (nums[index] === 0) {
+            var temp = nums[index];
+            nums[index] = nums[left];
+            nums[left] = temp;
+            left++;
+            index++;
+        } else if (nums[index] === 2) {
+            var temp = nums[index];
+            nums[index] = nums[right];
+            nums[right] = temp;
+            right--;
+        } else {
+            index++;
+        }
+    }
+};
