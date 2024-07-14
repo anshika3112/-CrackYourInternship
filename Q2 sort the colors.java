517a7dde84b5d6663ea4dec8ebc9eb7297346236
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-14 22:28:33 +0530

    Create Q2 sort the colors.java

---
+++ Q2 sort the colors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+       int n=nums.length;
+       int index=0;
+       int left=0;
+       int right=n-1;
+       while(index<=right)
+       {
+       if(nums[index]==0){
+             int temp=nums[index];
+             nums[index]=nums[left];
+             nums[left]=temp;
+              left++;
+              index++;
+        }
+        else if(nums[index]==2){
+            int temp=nums[index];
+            nums[index]=nums[right];
+            nums[right]=temp;
+            right--;
+        }
+        else
+        index++;
+       } 
+    }
+}
