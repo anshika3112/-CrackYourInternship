5e21335050890abe9b40e3c5dae3c315b3687f09
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-17 21:39:16 +0530

    Update Q8 two sum.cpp

--- Q8 two sum.cpp
+++ Q8 two sum.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        int i;
+        for ( i = 0; i < nums.size() - 1; ++i) {
+            int complement = target - nums[i];
+            auto it = find(nums.begin() + i + 1, nums.end(), complement);
+            
+            if (it != nums.end()) {
+                ans.push_back(i);
+                ans.push_back(it - nums.begin());
+                return ans;
+            }
+        }
+        
+        return ans; // Return empty vector if no pair found
+    }
+};
 
