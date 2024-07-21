390cca1b734d78c358423159a31bb85a9a40e705
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-21 23:20:08 +0530

    Update Q6 best time to sell and buy.js

--- Q6 best time to sell and buy.js
+++ Q6 best time to sell and buy.js
@@ -1,1 +1,25 @@
+var maxProfit = function(prices) {
+    function maxProfitFinder(prices, i, minPrice, maxProfit) {
+        if (i === prices.length) {
+            return maxProfit;
+        }
+        
+        // Update minPrice and maxProfit
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        }
+        const tProfit = prices[i] - minPrice;
+        if (tProfit > maxProfit) {
+            maxProfit = tProfit;
+        }
+        
+        // Recursive call to the next index
+        return maxProfitFinder(prices, i + 1, minPrice, maxProfit);
+    }
+    const minPrice = Infinity;
+    const maxProfit = 0;
+    return maxProfitFinder(prices, 0, minPrice, maxProfit);
+};
+
+
 
