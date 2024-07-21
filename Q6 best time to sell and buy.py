adb822b368933d2f30e47e9705f0ae93507b84ed
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-21 23:16:11 +0530

    Update Q6 best time to sell and buy.py

--- Q6 best time to sell and buy.py
+++ Q6 best time to sell and buy.py
@@ -1,1 +1,18 @@
+class Solution:
+    def maxProfitFinder(self, prices, i, minPrice, maxProfit):
+        if i == len(prices):
+            return maxProfit
+        if prices[i] < minPrice:
+            minPrice = prices[i]
+        tProfit = prices[i] - minPrice
+        if tProfit > maxProfit:
+            maxProfit = tProfit
+        return self.maxProfitFinder(prices, i + 1, minPrice, maxProfit)
+
+    def maxProfit(self, prices):
+        minPrice = float('inf')
+        maxProfit = float('-inf')
+        return self.maxProfitFinder(prices, 0, minPrice, maxProfit)
+
+
 
