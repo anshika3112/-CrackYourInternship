bae9074426c6b453edae8467b00b323dc69d90a3
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-21 23:16:42 +0530

    Update Q6 best time to sell and busy.java

--- Q6 best time to sell and busy.java
+++ Q6 best time to sell and busy.java
@@ -1,1 +1,18 @@
+class Solution {
+    public void maxProfitFinder(int[] prices, int i, int[] minPrice, int[] maxProfit) {
+        if (i == prices.length) return;
+        if (prices[i] < minPrice[0]) minPrice[0] = prices[i];
+        int tProfit = prices[i] - minPrice[0];
+        if (tProfit > maxProfit[0]) maxProfit[0] = tProfit;
+        maxProfitFinder(prices, i + 1, minPrice, maxProfit);
+    }
+
+    public int maxProfit(int[] prices) {
+        int[] minPrice = { Integer.MAX_VALUE };
+        int[] maxProfit = { Integer.MIN_VALUE };
+        maxProfitFinder(prices, 0, minPrice, maxProfit);
+        return maxProfit[0];
+    }
+
+}
 
