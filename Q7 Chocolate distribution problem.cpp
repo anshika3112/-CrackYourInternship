aafe77817493dcce886967ed1287a1daf9c1ff78
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-22 23:45:27 +0530

    Update Q7 Chocolate distribution problem.cpp

--- Q7 Chocolate distribution problem.cpp
+++ Q7 Chocolate distribution problem.cpp
@@ -1,1 +1,16 @@
-
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+    sort(a.begin(),a.end());
+    long long min=LLONG_MAX;
+    int i=0;
+    for(int i=0;i+m-1<n;i++)
+    {
+        int max=a[i+m-1]-a[i];
+        if(max<min)
+        min=max;
+    }
+    return min;
+    }   
+};
