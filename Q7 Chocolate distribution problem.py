4f1e1094af13bcebaa684b86ab795fcbaeb821ad
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-23 16:00:28 +0530

    Update Q7 Chocolate distribution problem.py

--- Q7 Chocolate distribution problem.py
+++ Q7 Chocolate distribution problem.py
@@ -1,1 +1,16 @@
+class Solution:
 
+    def findMinDiff(self, A, N, M):
+        # Sort the array
+        A.sort()
+
+        # Initialize the minimum difference
+        min_diff = float('inf')
+
+        # Traverse the sorted list and find the minimum difference between the maximum and minimum of M elements
+        for i in range(N - M + 1):
+            diff = A[i + M - 1] - A[i]
+            if diff < min_diff:
+                min_diff = diff
+
+   
