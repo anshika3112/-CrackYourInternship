fe74aaabb4b0960e4e5a8c618db644336a7f8293
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-23 15:59:49 +0530

    Update Q7 Chocolate distribution problem.js

--- Q7 Chocolate distribution problem.js
+++ Q7 Chocolate distribution problem.js
@@ -1,1 +1,18 @@
 
+class Solution{
+    findMinDiff(arr,n,m){
+        //code here
+        arr.sort((a,b)=>a-b);
+        let minDiff = Infinity;
+
+        // Traverse the sorted list and find the minimum difference between the maximum and minimum of m elements
+        for (let i = 0; i + m - 1 < n; i++) {
+            let diff = arr[i + m - 1] - arr[i];
+            if (diff < minDiff) {
+                minDiff = diff;
+            }
+        }
+
+        return minDiff;
+    }
+}
