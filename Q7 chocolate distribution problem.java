ac4ce216046d9792851f7f06defd73d1da99a509
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-23 15:38:58 +0530

    Update Q7 chocolate distribution problem.java

--- Q7 chocolate distribution problem.java
+++ Q7 chocolate distribution problem.java
@@ -1,1 +1,17 @@
 
+class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        // your code here
+        Collections.sort(a);
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i+m-1<n;i++)
+        {
+            int max=a.get(i+m-1)-a.get(i);
+            if(max<min)
+            min=max;
+        }
+        return min;
+    }
+}
