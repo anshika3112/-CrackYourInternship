3debb0c20f8023abf2d5c902490a20cdc35021a3
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-29 23:15:12 +0530

    Update Q19 merge sorted array .js

--- Q19 merge sorted array .js
+++ Q19 merge sorted array .js
@@ -1,1 +1,9 @@
 
+var merge = function(nums1, m, nums2, n) {
+    for (let j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        // Sort the merged array
+        nums1.sort((a, b) => a - b);
+};
