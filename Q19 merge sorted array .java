91536f7955f8081205b908b35af0ff58ebf55b63
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-29 23:13:55 +0530

    Update Q19 merge sorted array .java

--- Q19 merge sorted array .java
+++ Q19 merge sorted array .java
@@ -1,1 +1,13 @@
 
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Copy elements from nums2 to the end of nums1
+        for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        // Sort the merged array
+        Arrays.sort(nums1);
+    }
+}
+
