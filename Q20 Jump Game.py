a9b6739e6902ad629a8cd753e61a4d1727e3810a
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-07-30 22:22:48 +0530

    Update Q20 Jump Game.py

--- Q20 Jump Game.py
+++ Q20 Jump Game.py
@@ -1,1 +1,12 @@
+class Solution:
+    def canJump(self, nums) :
+        n = len(nums)
+        max_reach = 0
+        for i in range(n):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, i + nums[i])
+            if max_reach >= n - 1:
+                return True
+        return False
 
