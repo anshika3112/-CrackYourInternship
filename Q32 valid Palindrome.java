3024b78971450d6f737cb44fe9141306335da350
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-08-08 00:07:49 +0530

    Update Q32 valid Palindrome.java

--- Q32 valid Palindrome.java
+++ Q32 valid Palindrome.java
@@ -1,1 +1,28 @@
+class Solution {
+    public boolean palin(String s, int i, int j) {
+        while (j >= i) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            } else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (j >= i) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return palin(s, i + 1, j) || palin(s, i, j - 1);
+            } else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
 
