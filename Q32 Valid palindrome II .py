bead7c068d4d92b876a14844f273f6082bfd9195
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-08-08 00:08:10 +0530

    Update Q32 Valid palindrome II .py

--- Q32 Valid palindrome II .py
+++ Q32 Valid palindrome II .py
@@ -1,1 +1,21 @@
+class Solution:
+    def palin(self, s, i, j):
+        while j >= i:
+            if s[i] != s[j]:
+                return False
+            else:
+                i += 1
+                j -= 1
+        return True
+
+    def validPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+        while j >= i:
+            if s[i] != s[j]:
+                return self.palin(s, i + 1, j) or self.palin(s, i, j - 1)
+            else:
+                i += 1
+                j -= 1
+        return True
 
