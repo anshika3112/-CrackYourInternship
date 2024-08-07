5fee60b074a930e391b7455caa7e11b8814b3630
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-08-08 00:07:20 +0530

    Update Q32 Valid palindrome II.js

--- Q32 Valid palindrome II.js
+++ Q32 Valid palindrome II.js
@@ -1,1 +1,27 @@
 
+    var function palin(s, i, j) {
+        while (j >= i) {
+            if (s[i] !== s[j]) {
+                return false;
+            } else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+
+    var function validPalindrome(s) {
+        let i = 0;
+        let j = s.length - 1;
+        while (j >= i) {
+            if (s[i] !== s[j]) {
+                return this.palin(s, i + 1, j) || this.palin(s, i, j - 1);
+            } else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+
